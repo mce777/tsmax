ea234d26d0be6aaf075c77c78c84c3e8c89d6671
marebe <mark.eberhard@sinnerschrader.com>
2016-12-14 16:46:24 +0100

    finished

---
+++ 12_ts_decorators01.ts
@@ -0,0 +1,69 @@
+// decorators are a ts feature (an experimental one)
+// this example here is printing the constructor fn of the class
+function logged(constructorFn: Function) {
+	console.log(constructorFn);
+}
+
+@logged  //this attaches it to the class
+class Person {
+	constructor() {
+		console.log("Hi");
+	}
+}
+
+// Factory
+function logging(value: boolean) {
+	return value ? logged : null;
+}
+
+@logging(true)
+class Car {
+
+}
+
+// Advanced
+function printable(constructorFn: Function) {
+	constructorFn.prototype.print = function() {
+		console.log(this);
+	}
+}
+
+// can have multiple decorators
+@logging(true)
+@printable
+class Plant {
+	name = "Green Plant";
+}
+const plant = new Plant();
+(<any>plant).print();
+
+// Method Decorator
+function editable(value: boolean) {
+	return function(target: any, propName: string, descriptor: PropertyDescriptor) {
+		descriptor.writable = value;
+	}
+}
+
+class Project {
+	projectName: string;
+
+	constructor(name: string) {
+		this.projectName = name;
+	}
+
+	// here through the decorator we are making calcBudget() uneditable
+	// so changing it below will not work
+	@editable(false)
+	calcBudget() {
+		console.log(1000);
+	}
+}
+
+const project = new Project("super duper project");
+project.calcBudget();
+project.calcBudget = function() {
+	console.log(7777);
+};
+project.calcBudget();
+
+// property and parameter decorators don't seem extremely useful
---
+++ 13_ts_typings.md
@@ -0,0 +1,15 @@
+As of Typescript 2.0 you can install typings simply via NPM
+
+for example...
+
+`npm install --save-dev @types/jquery`
+
+we're still using 'definitely typed' Github repo. This is just a simpler way to do it. 
+It gets added to `package.json` and the `node_module/@types` folder. 
+
+For **React**, run this (sets up react and react-dom):
+
+`npm install --save-dev @types/react @types/react-dom`
+
+
+
