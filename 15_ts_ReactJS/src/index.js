1baf4163ab309906b43778d1a7d7eff5448bac4e
Mark <mark@Marks-MacBook-Pro.local>
2017-03-20 22:46:31 +0100

    updates

---
+++ 15_ts_ReactJS/src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var React = require("react");
+var ReactDOM = require("react-dom");
+var Home_1 = require("./components/Home");
+ReactDOM.render(React.createElement(Home_1.Home, { name: "Max", age: 27 }), document.getElementById("app"));
--- 15_ts_ReactJS/src/index.tsx
+++ 15_ts_ReactJS/src/index.tsx
@@ -3,4 +3,6 @@ import * as ReactDOM from "react-dom";
 
 import { Home } from "./components/Home";
 
-ReactDOM.render(<Home name="Max" age={27}/>, document.getElementById("app"));
+ReactDOM.render(
+  <Home name="Max" age={27}/>, document.getElementById("app")
+);
